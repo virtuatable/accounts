71aa24898c88ba9f41f13860c87086356e9c358a
Vincent COURTOIS <vcs@suneris.com>
2019-07-24 17:44:04 +0200

    Fix #7 as a user I want to be able to be linked to some groups.

--- controllers/accounts_controller.rb
+++ controllers/accounts_controller.rb
@@ -43,49 +43,27 @@ class AccountsController < Arkaan::Utils::Controllers::Checked
     end
   end
 
-  declare_route('patch', '/own/phones') do
-    session = check_session('add_phone')
-    check_presence('number', 'privacy', route: 'add_phone')
-    custom_error(400, 'add_phone.privacy.wrong_value') if !['players', 'private', 'public'].include?(params['privacy'])
-    phone = Arkaan::Phone.new(phone_parameters.merge(account: session.account))
-    if phone.save
-      halt 201, {message: 'created', item: Decorators::Phone.new(phone).to_h}.to_json
-    else
-      model_error(phone, 'add_phone')
+  declare_premium_route('put', '/:id') do
+    session = check_session('update')
+    account = Arkaan::Account.where(id: params['id']).first
+    custom_error 404, 'update.account_id.unknown' if account.nil?
+    if params.has_key? 'groups'
+      custom_error 404, 'update.group_id.unknown' if params['groups'].any? do |group_id|
+        Arkaan::Permissions::Group.where(id: group_id).first.nil?
+      end
+      account.group_ids = params['groups']
+      if account.save
+        halt 200, {message: 'updated', item: Decorators::Account.new(account).to_h}.to_json
+      else
+        model_error(account, 'update')
+      end
     end
-  end
-
-  declare_route('delete', '/own/phones/:phone_id') do
-    session = check_session('phone_deletion')
-    phone = session.account.phones.where(id: params['phone_id']).first
-    if phone.nil?
-      custom_error(404, 'phone_deletion.phone_id.unknown')
-    else
-      phone.delete
-      halt 200, {message: 'deleted'}.to_json
-    end
-  end
-
-  # Checks if the session ID is given in the parameters and if the session exists.
-  # @param action [String] the action used to get the errors from the errors file.
-  # @return [Arkaan::Authentication::Session] the session when it exists.
-  def check_session(action)
-    check_presence('session_id', route: action)
-    session = Arkaan::Authentication::Session.where(token: params['session_id']).first
-    if session.nil?
-      custom_error(404, "#{action}.session_id.unknown")
-    end
-    return session
   end
 
   # Selects the parameters suited to create an account.
   # @return [Hash<String, Object>] the hash composed of the selected keys.
   def account_parameters
     return select_params('username', 'password', 'password_confirmation', 'firstname', 'lastname', 'email', 'gender', 'language')
-  end
-
-  def phone_parameters
-    return select_params('number', 'privacy')
   end
 
   def select_params(*fields)
